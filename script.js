3988d8b534a672b410a6d04d0354b614cd69ae75
Manvendra Singh <mjdeora@gmail.com>
2020-01-10 23:01:34 +0530

    Pouch db added, Create handled

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
 
 $(document).ready(function () {
+    let db = new PouchDB('contacts');
+
+    db.changes({
+        since: 'now',
+        live: true
+    }).on('change', showTodos);
+
+    function showTodos() {
+        db.allDocs({
+            include_docs: true,
+            descending: true
+        }).then(function (result) {
+            console.log(result)
+        }).catch(function (err) {
+            console.log(err);
+        });
+    }
+
     $('form').submit(function (e) {
         e.preventDefault();
-        let data = $('form').serializeArray()
-        console.log(data)
+        let formData = $('form').serializeArray()
+
+        let jsonData = {};
+        for (let i = 0; i < formData.length; i++) {
+            jsonData[formData[i]['name']] = formData[i]['value'];
+        }
+
+        db.post(jsonData).then(function (response) {
+            console.log('Success ', response);
+        }).catch(function (err) {
+            console.log(err);
+        });
     });
 
     let emailElement = $('#emailElement').clone()
     let contactElement = $('#contactElement').clone()
 
     $('#addAnotherEmail').click(function () {
-        $('#emailElement').after(emailElement)
+        $('#emailContainer').append(emailElement)
     })
 
     $('#addAnotherContact').click(function () {
