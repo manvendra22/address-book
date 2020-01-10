9ef523b23bd22819fb81931ac9ad493d9048fb8c
Manvendra Singh <mjdeora@gmail.com>
2020-01-10 21:16:17 +0530

    Form validation completed

--- script.js
+++ script.js
@@ -2,10 +2,18 @@
 $(document).ready(function () {
     $('form').submit(function (e) {
         e.preventDefault();
-    });
-
-    $('#addData').click(function () {
         let data = $('form').serializeArray()
         console.log(data)
     });
+
+    let emailElement = $('#emailElement').clone()
+    let contactElement = $('#contactElement').clone()
+
+    $('#addAnotherEmail').click(function () {
+        $('#emailElement').after(emailElement)
+    })
+
+    $('#addAnotherContact').click(function () {
+        $('#contactElement').after(contactElement)
+    })
 })
