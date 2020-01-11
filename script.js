8cd08ce3359135b4a83e80a2ad54308606090ae4
Manvendra <mjdeora@gmail.com>
2020-01-11 16:27:34 +0530

    Update functionality added

--- script.js
+++ script.js
@@ -1,54 +1,91 @@
+let db = new PouchDB('contacts');
+
+if(db) {
+    showContacts()
+}
+
+db.changes({
+    since: 'now',
+    live: true
+}).on('change', showContacts);
+
+function showContacts() {
+    db.allDocs({
+        include_docs: true,
+        descending: true
+    }).then(function (result) {
+        console.log('Fetched ', result)
+        createContact(result.rows)
+    }).catch(function (err) {
+        console.log(err);
+    });
+}
+
+function createContact(data) {
+    let elements = ''
+
+    data.forEach(data => {
+        const { id, doc } = data
+        const { firstName, lastName, contact, email } = doc
+
+        let element = `<div class="list-item">
+                    <img src="/icons/cross.svg" class="icon cross" alt="" srcset="" data-id=${id}>
+                    <div class="data-container">
+                        <p>${firstName} ${lastName}</p>
+                        <p>${contact} (3 more)</p>
+                        <p>${email} (1 more)</p>
+                    </div>
+                    <div class="icons-container">
+                        <img src="/icons/eye.svg" class="icon eye" alt="" srcset="" data-id=${id}>
+                        <img src="/icons/edit.svg" class="icon edit" alt="" srcset="" data-id=${id}>
+                        <img src="/icons/email.svg" class="icon" alt="" srcset="">
+                        <img src="/icons/call.svg" class="icon" alt="" srcset="">
+                    </div>
+                 </div>`
+
+        elements += element
+    })
+
+    $('.list-container').html(elements)
+}
+
+function fillDataInViewModal(data) {
+    const { firstName, lastName, contact, contactType, dob, email } = data
+
+    $('#contactViewModal').modal('show');
+
+    $("#view_name").text(`${firstName} ${lastName}`);
+    $("#view_contact").text(contact);
+    $("#view_contact_type").text(contactType);
+    $("#view_dob").text(dob);
+    $("#view_email").text(email);
+}
+
+function fillDataInEditModal(data) {
+    const { firstName, lastName, contact, contactType, dob, email, _id, _rev } = data
+
+    $("form").trigger("reset");
+    $('#contactFormModal').modal('show');
+
+    $("form").attr("data-id", _id);
+    $("form").attr("data-rev", _rev);
+
+    $("#first_name").val(firstName);
+    $("#last_name").val(lastName);
+    $("#contact").val(contact);
+    $("#contact_type").val(contactType);
+    $("#dob").val(dob);
+    $("#email").val(email);
+}
+
+function addContact() {
+    $("form").trigger("reset");
+    $("form").removeAttr("data-id");
+
+    $('#contactFormModal').modal('show');
+}
 
 $(document).ready(function () {
-    let db = new PouchDB('contacts');
-
-    if(db) {
-        showContacts()
-    }
-
-    db.changes({
-        since: 'now',
-        live: true
-    }).on('change', showContacts);
-
-    function showContacts() {
-        db.allDocs({
-            include_docs: true,
-            descending: true
-        }).then(function (result) {
-            console.log('Fetched ', result)
-            createContact(result.rows)
-        }).catch(function (err) {
-            console.log(err);
-        });
-    }
-
-    function createContact(datas) {
-        let elements = ''
-
-        datas.forEach(data => {
-            const { id, doc } = data
-            const { firstName, lastName, contact, email } = doc
-
-            let element = `<div class="list-item">
-                        <img src="/icons/cross.svg" class="icon cross" alt="" srcset="" data-id=${id}>
-                        <div class="data-container">
-                            <p>${firstName} ${lastName}</p>
-                            <p>${contact} (3 more)</p>
-                            <p>${email} (1 more)</p>
-                        </div>
-                        <div class="icons-container">
-                            <img src="/icons/eye.svg" class="icon eye" alt="" srcset="" data-id=${id}>
-                            <img src="/icons/email.svg" class="icon" alt="" srcset="">
-                            <img src="/icons/call.svg" class="icon" alt="" srcset="">
-                        </div>
-                     </div>`
-
-            elements += element
-        })
-
-        $('.list-container').html(elements)
-    }
 
     $(document).on('click', '.cross', function (e){
         let id = $(this).attr("data-id")
@@ -67,44 +104,56 @@ $(document).ready(function () {
         
         db.get(id).then(function(doc) {
             console.log('doc ', doc)
-            fillDataInModal(doc)
+            fillDataInViewModal(doc)
           }).catch(function (err) {
             console.log(err);
         });          
     })
 
-    function fillDataInModal(data) {
-        const { firstName, lastName, contact, contactType, dob, email } = data
-
-        $('#contactViewModal').modal('show');
-
-        $("#view_name").text(`${firstName} ${lastName}`);
-        $("#view_contact").text(contact);
-        $("#view_contact_type").text(contactType);
-        $("#view_dob").text(dob);
-        $("#view_email").text(email);
-    }
+    $(document).on('click', '.edit', function (e){
+        let id = $(this).attr("data-id")
+        
+        db.get(id).then(function(doc) {
+            console.log('doc ', doc)
+            fillDataInEditModal(doc)
+          }).catch(function (err) {
+            console.log(err);
+        });          
+    })
 
     $('form').submit(function (e) {
+        let id = $(this).attr("data-id")
+
         e.preventDefault();
-        let formData = $('form').serializeArray()
 
         $('#contactFormModal').modal('hide');
 
+        let formData = $('form').serializeArray()
         let jsonData = {};
+
         for (let i = 0; i < formData.length; i++) {
             jsonData[formData[i]['name']] = formData[i]['value'];
         }
 
-        db.post(jsonData).then(function (response) {
-            $("form").trigger("reset");
-            console.log('Added ', response);
-        }).catch(function (err) {
-            console.log(err);
-        });
-    });
+        if(id) {
+            let rev = $(this).attr("data-rev")
 
-    
+            jsonData._id = id
+            jsonData._rev = rev
+
+            db.put(jsonData).then(function (response) {
+                console.log('Updated ', response);
+            }).catch(function (err) {
+                console.log(err);
+            });
+        } else {
+            db.post(jsonData).then(function (response) {
+                console.log('Added ', response);
+            }).catch(function (err) {
+                console.log(err);
+            });
+        }
+    });
 
     // let emailElement = $('#emailElement').clone()
     // let contactElement = $('#contactElement').clone()
@@ -117,4 +166,3 @@ $(document).ready(function () {
     //     $('#contactElement').after(contactElement)
     // })
 })
-
